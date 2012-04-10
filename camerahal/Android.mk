0b6d3912ed530beaf3aec5cfa19e8d7b4cc55bb0
TwistedUmbrella <twistedumbrella@gmail.com>
2012-04-10 11:01:52 -0400

    Remove deprecated camera flags for default parameters

--- camerahal/Android.mk
+++ camerahal/Android.mk
@@ -35,9 +35,6 @@ endif
 ifeq ($(BOARD_USE_REVERSE_FFC), true)
     LOCAL_CFLAGS += -DREVERSE_FFC
 endif
-ifeq ($(TARGET_USE_CAF_PARAMS), true)
-    LOCAL_CFLAGS += -DCAF_PARAMS
-endif
 
 include $(BUILD_SHARED_LIBRARY)
 
